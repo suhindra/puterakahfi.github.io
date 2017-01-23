66417d95bedd66317fccf3cb4710ae018e3939e9
putera kahfi <pkahfi@localhost.localdomain>
2017-01-23 20:00:41 +0700

    update vue

---
+++ src/Landing.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Landing'
+
+export default {
+  name: 'app',
+  components: {
+    Landing
+  }
+}
+</script>
+
